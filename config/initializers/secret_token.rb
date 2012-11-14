0d0c2d22eda29ac82a7b005b8785ddd5886bb638
Chris Johnson <cjohnson@foodlogiq.com>
2012-11-14 09:23:26 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DocTwitApp::Application.config.secret_token = '20f9f638817b0abbdd7a28d9b7d54d570341777c5b16fa15a60205991912698e1f195d7bed809d0b7acd6c5e3a6ab8e4757b349cf189d24237de7818022c7dea'
